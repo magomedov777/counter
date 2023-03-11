621186ff1ff9d5cd210f3d5f9d0ffe20c21bcc62
Arkadiy <alibullterrier@gmail.com>
2023-03-11 16:03:52 +0200

    new styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,12 +6,13 @@ function App() {
   let clearValue = () => {setNum(0)}
   let minusNum = () => {setNum(--num)}
   let[num, setNum] = useState(0)
+
   return (
     <div className="App">
      <h1>{num}</h1>
-     <button onClick={plusNum}>PLUS</button>
-     <button onClick={clearValue}>CLEAR</button>
-     <button onClick={minusNum}>MINUS</button>
+      <button className='plus' onClick={plusNum}>PLUS</button>
+      <button className='clear' onClick={clearValue}>CLEAR</button>
+      <button className='minus' onClick={minusNum}>MINUS</button>
     </div>
   );
 }
