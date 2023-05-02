8ae350584d39800d2cf6fce3d8952d8b8b5605eb
Arkadiy <alibullterrier@gmail.com>
2023-05-02 06:52:55 +0300

    create useState

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -4,6 +4,7 @@ import { Buttons } from "./Buttons";
 import { Screen } from "./Screen";
 
 export const Counter: React.FC = ()  => {
+  let [count, setCount] = useState(0);
 
   const incrementHandler = () => setCount(++count)
 
